a22fb8e585da29d7a709505adf6cfd5e5d77842a
Nicobrave <nicobrave@icloud.com>
2022-01-03 16:48:28 -0300

    Pirmer registro

---
+++ assets/js/main2.js
@@ -0,0 +1,34 @@
+var settings = {
+  "url": "https://www.cannabisreports.com/api/v1.0/strains?a5ccdeba4dmsh791a99486650a7bp137d53jsn9b5dfca70352=x-rapidapi-key",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Cookie": "XSRF-TOKEN=eyJpdiI6IjdGV2lrekFnd0I4NnBzblJGVEFrdmc9PSIsInZhbHVlIjoicmJXeTNvM096eVVOK09VZmFcL0Q0dXlkSWhyMFpYRFhKUmpERytJSXUwdVRKMk81VlQ2UDZ4QjZCR3gwaEhFWDhkMk95S3hrVjZpcXQ0TjhjWkU1V1BRPT0iLCJtYWMiOiJiZGFhMzBhZDFhMzk3NjMwMDc1Y2Q1NThjZWE2MGQ3NjQ3Yzc3MTQ5MmYyNjBhZGE0M2YyNWNjODRiMDBhZTUwIn0%3D; laravel_session=eyJpdiI6ImtjYjRFR25ZZk42dXFhVXF4XC8rVkxRPT0iLCJ2YWx1ZSI6IjNIRkRkUXlpTnZsb0ZQSGNmZ0VPRGtmUkVcL2VDM0VqbVpnQldHY3d1bFJJcHhnZlhKYXRIc1UySVR4VE9iU21PcWhTZ2pFY0RSMVdoVis2c2pGdUhzZz09IiwibWFjIjoiNzY5ZTI4YWFhMjZhZmE1YmY4NzdkYjY3M2UwYzdlM2M3ZGFiZjY3NGY2ODkxODBlNDA0MDc0ZjZlY2ZiNWQzZiJ9"
+  },
+};
+
+
+/* // Esta función realiza una busqueda aleatoria de un objeto y entrega como resultado el nombre de la cepa.
+
+(function() {
+  $('btn').on('submit', function(e) {
+    e.preventDefault()
+    $.ajax(settings).done(function (data) {
+      console.log(data);
+      const strain = data[ Math.floor(Math.random() * 9007) ]
+
+      const buscar = $(this).find(data)
+      const random =
+      getCepa(data.name)
+    });
+    
+    
+    
+  })
+})()
+
+// var random_number = Math.floor(Math.random() * 10);
+// console.log(random_number);
+
+
+*/
